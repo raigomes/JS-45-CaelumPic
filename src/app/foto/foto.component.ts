e1cb89c1f58672f9304ffd9f82464932fcf0d494
raigomes <rai.gomes.ismart@gmail.com>
2017-08-17 19:27:28 -0300

    Reduzindo títulos grandes no Painel

--- src/app/foto/foto.component.ts
+++ src/app/foto/foto.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'foto',
   templateUrl: './foto.component.html'
 })
-export class FotoComponent {
+export class FotoComponent implements OnInit {
   @Input() titulo:string = '';
   @Input() url:string = '';
   descricao:string = '';
+
+  ngOnInit() {
+    this.titulo = this.titulo.length > 7
+                  ? this.titulo.substr(0,7) + '...'
+                  : this.titulo;
+  }
 }
